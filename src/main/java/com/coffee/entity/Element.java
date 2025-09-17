f49aecedebc13f7f911442a0dd3e58532cac06e8
김기태 <aqz103@naver.com>
2025-09-17 17:01:34 +0900

    initial commit

---
+++ src/main/java/com/coffee/entity/Element.java
@@ -0,0 +1,33 @@
+package com.coffee.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Setter
+@Getter
+@ToString
+
+public class Element {
+    private int id;
+    private String name;
+    private int price;
+    private String category;
+    private int stock;
+    private String image;
+    private String description;
+
+    public Element(int id, String name, int price, String category, int stock, String image, String description) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.category = category;
+        this.stock = stock;
+        this.image = image;
+        this.description = description;
+    }
+
+    public Element() {
+
+    }
+}
