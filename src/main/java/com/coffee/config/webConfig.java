8013cf1fdea90cf4787da0a166e8670d98247387
김기태 <aqz103@naver.com>
2025-09-18 17:22:43 +0900

    collabo_spring 2025/0918

--- src/main/java/com/coffee/config/webConfig.java
+++ src/main/java/com/coffee/config/webConfig.java
@@ -1,7 +1,9 @@
 package com.coffee.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration // 해당 클래스를 객체로 만들어 주되, 이 파일은 설정용 파일입니다.
@@ -11,8 +13,19 @@ public class webConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // 모든경로 허용
                 .allowedOrigins("http://localhost:3000") // react 포트
-                .allowedMethods("GET","POST","DELETE","OPTIONS","PATCH") //허용할 메소드
+                .allowedMethods("GET","POST", "Put", "DELETE","OPTIONS","PATCH") //허용할 메소드
                 .allowCredentials(true); //쿠키 전송 허용
 
+    }
+
+    //application.prorerties 파일에서 uploadPath 항목의 값을 변수에 할당해 줍니다.
+    @Value("${uploadPath}")
+    private String uploadPath ; // file///c:/shop/images/
+
+    @Override //외부에서 /images/**"란 요청이 들어오면, uploadPath를 한번 찾아 보세요.
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry
+                .addResourceHandler("/images/**")
+                .addResourceLocations(uploadPath);
     }
 }
